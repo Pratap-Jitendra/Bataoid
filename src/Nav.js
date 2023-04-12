9f66763aefe8ed4e1ca7a9d5a2a9739d7a6cac50
Pratap-Jitendra <jitendra.pratap205@gmail.com>
2023-04-12 05:01:36 -0700

    color change

--- src/Nav.js
+++ src/Nav.js
@@ -7,8 +7,8 @@ const Nav = () => {
     <div className='navbar'>
       <ul>
         <li><img src="Image/logo.jfif" alt="first" /></li>
-        <li><Link to="/"><img src="Image/cong.png" alt="name" /></Link></li>
-        <li><img src="Image/gaj.jpeg" alt="name" />
+        <li><Link to="/"><img src="Image/cong.jpeg" alt="name" /></Link></li>
+        <li><img src="Image/gaj.jpg" alt="name" />
           <ul className='dropdown'>
             <li><Link to="/gaj"><img src="Image/left.webp" alt="name" /></Link></li>
             <li><Link to="/second"><img src="Image/right.webp" alt="name" /></Link></li>
